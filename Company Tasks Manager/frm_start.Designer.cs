b3780ec570637c02a91ccae551406dcbf4fc4004
hady khan <Hady.KHann@gmail.com>
2019-12-19 16:02:11 +0330

    Add project files.

---
+++ Company Tasks Manager/frm_start.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Company_Tasks_Manager
+{
+    partial class frm_start
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbl_status = new System.Windows.Forms.Label();
+            this.lbl_number = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(156, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(351, 393);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lbl_status
+            // 
+            this.lbl_status.AutoSize = true;
+            this.lbl_status.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_status.Location = new System.Drawing.Point(43, 664);
+            this.lbl_status.Name = "lbl_status";
+            this.lbl_status.Size = new System.Drawing.Size(0, 25);
+            this.lbl_status.TabIndex = 3;
+            // 
+            // lbl_number
+            // 
+            this.lbl_number.AutoSize = true;
+            this.lbl_number.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_number.Location = new System.Drawing.Point(560, 702);
+            this.lbl_number.Name = "lbl_number";
+            this.lbl_number.Size = new System.Drawing.Size(50, 25);
+            this.lbl_number.TabIndex = 4;
+            this.lbl_number.Text = "1 / 6";
+            // 
+            // frm_start
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.ClientSize = new System.Drawing.Size(645, 736);
+            this.Controls.Add(this.lbl_number);
+            this.Controls.Add(this.lbl_status);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "frm_start";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "start_frm";
+            this.Load += new System.EventHandler(this.frm_start_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbl_status;
+        private System.Windows.Forms.Label lbl_number;
+
+    }
+}
---
+++ Company Tasks Manager/frm_start.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Company_Tasks_Manager
+{
+    public partial class frm_start : Form
+    {
+        public frm_start()
+        {
+            InitializeComponent();
+        }
+
+        private void frm_start_Load(object sender, EventArgs e)
+        {
+            FormBorderStyle = FormBorderStyle.None;
+
+            int ranservices = 0;
+            int time = 3000;
+            while (ranservices != 6)
+            {
+                ranservices = 0;
+
+
+
+                if (Services.Servicestatus("MSSQLFDLauncher"))
+                {
+                    lbl_status.Text = "starting MSSQLFDLauncher"; lbl_status.Refresh();
+                    Services.StartService("MSSQLFDLauncher", time);
+                    ranservices++;
+                    lbl_number.Text = ranservices + " / 6"; lbl_number.Refresh();
+                }
+                else
+                {
+                    ranservices++;
+                    lbl_status.Text = "MSSQLFDLauncher was already started"; lbl_status.Refresh();
+                    lbl_number.Text = ranservices + " / 6"; lbl_number.Refresh();
+                    Thread.Sleep(time);
+                }
+
+
+
+
+                if (Services.Servicestatus("MSSQLSERVER"))
+                {
+                    lbl_status.Text = "starting MSSQLSERVER"; lbl_status.Refresh();
+                    Services.StartService("MSSQLSERVER", time);
+                    ranservices++;
+                    lbl_number.Text = ranservices + " / 6"; lbl_number.Refresh();
+                }
+                else
+                {
+                    ranservices++;
+                    lbl_status.Text = "MSSQLSERVER was already started"; lbl_status.Refresh();
+                    lbl_number.Text = ranservices + " / 6"; lbl_number.Refresh();
+                    Thread.Sleep(time);
+                }
+
+
+
+
+                if (Services.Servicestatus("SQLSERVERAGENT"))
+                {
+                    lbl_status.Text = "starting SQLSERVERAGENT"; lbl_status.Refresh();
+                    Services.StartService("SQLSERVERAGENT", time);
+                    lbl_number.Text = ranservices + " / 6"; lbl_number.Refresh();
+                    ranservices++;
+                }
+                else
+                {
+                    ranservices++;
+                    lbl_status.Text = "SQLSERVERAGENT was already started"; lbl_status.Refresh();
+                    lbl_number.Text = ranservices + " / 6"; lbl_number.Refresh();
+                    Thread.Sleep(time);
+                }
+
+
+
+
+                if (Services.Servicestatus("MSSQLServerOLAPService"))
+                {
+                    lbl_status.Text = "starting MSSQLServerOLAPService"; lbl_status.Refresh();
+                    Services.StartService("MSSQLServerOLAPService", time);
+                    lbl_number.Text = ranservices + " / 6"; lbl_number.Refresh();
+                    ranservices++;
+                }
+                else
+                {
+                    ranservices++;
+                    lbl_status.Text = "MSSQLServerOLAPService was already started"; lbl_status.Refresh();
+                    lbl_number.Text = ranservices + " / 6"; lbl_number.Refresh();
+                    Thread.Sleep(time);
+                }
+
+
+
+
+                if (Services.Servicestatus("SSASTELEMETRY"))
+                {
+                    lbl_status.Text = "starting SSASTELEMETRY"; lbl_status.Refresh();
+                    Services.StartService("SSASTELEMETRY", time);
+                    lbl_number.Text = ranservices + " / 6"; lbl_number.Refresh();
+                    ranservices++;
+                }
+                else
+                {
+                    ranservices++;
+                    lbl_status.Text = "SSASTELEMETRY was already started"; lbl_status.Refresh();
+                    lbl_number.Text = ranservices + " / 6"; lbl_number.Refresh();
+                    Thread.Sleep(time);
+                }
+
+
+
+
+                if (Services.Servicestatus("SQLBrowser"))
+                {
+                    lbl_status.Text = "starting SQLBrowser"; lbl_status.Refresh();
+                    Services.StartService("SQLBrowser", time);
+                    lbl_number.Text = ranservices + " / 6"; lbl_number.Refresh();
+                    ranservices++;
+                }
+                else
+                {
+                    ranservices++;
+                    lbl_status.Text = "SQLBrowser was already started"; lbl_status.Refresh();
+                    lbl_number.Text = ranservices + " / 6"; lbl_number.Refresh();
+                    Thread.Sleep(time);
+                }
+
+            }
+
+            lbl_status.Text = "ALL SERVICES ARE STARTED ...."; lbl_status.Refresh();
+
+            frm_main frmm = new frm_main()
+            {
+                Opacity = 100
+            };
+            Thread.Sleep(time);
+            this.Close();
+
+
+        }
+        class Services
+        {
+            public static void StartService(string serviceName, int timeoutMilliseconds)
+            {
+                ServiceController service = new ServiceController(serviceName);
+
+                try
+                {
+                    TimeSpan timeout = TimeSpan.FromMilliseconds(timeoutMilliseconds);
+                    if ((service.Status.Equals(ServiceControllerStatus.Stopped)) || (service.Status.Equals(ServiceControllerStatus.StopPending)))
+                    {
+                        service.Start();
+                    }
+
+                    service.WaitForStatus(ServiceControllerStatus.Running, timeout);
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+
+            }
+
+            public static bool Servicestatus(string serviceName)
+            {
+                ServiceController service = new ServiceController(serviceName);
+
+                try
+                {
+                    if ((service.Status.Equals(ServiceControllerStatus.Stopped)) || (service.Status.Equals(ServiceControllerStatus.StopPending)))
+                    {
+                        return true;
+                    }
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+
+                }
+
+                return false;
+            }
+        }
+
+
+    }
+}
